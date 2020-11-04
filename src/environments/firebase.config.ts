e584b242795fbf5d95b42d9d4ce78579ef5679e7
Prashant chauhan <dataoct@hotmail.com>
2020-11-04 12:46:37 +0530

    first commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyAnvFKlsTSZPaOl_nDo_8y_vSXMVNAPSCg",
+  authDomain: "angular-shoppingcart-84c67.firebaseapp.com",
+  databaseURL: "https://angular-shoppingcart-84c67.firebaseio.com",
+  projectId: "angular-shoppingcart-84c67",
+  storageBucket: "angular-shoppingcart-84c67.appspot.com",
+  messagingSenderId: "539087964520",
+  appId: "1:539087964520:web:44c8da4c77704b829baedd"
+};
